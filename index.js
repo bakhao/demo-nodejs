d8d85edc3480a82625c1e31fe6021721bde81290
bakha <djickel.diaw@systematix-qc.com>
2023-10-27 14:21:18 -0400

    change message index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => {
-  res.send('Hello Welcome to to nodeJs!');
+  res.send('Hello Welcome to my app nodeJs!');
 });
 
 app.listen(port, () => {
